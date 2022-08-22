432e1d6b10de6e78e743d0202c3be42d013f7d2b
Nastya <stushashilova@tut.by>
2022-08-22 20:13:58 +0300

    Task max and min

---
+++ Homework1_Task001/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите первое число:  ");
+int x = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine("Введите второе число:  ");
+int y = Convert.ToInt32 (Console.ReadLine ());
+if (x > y)
+{
+    Console.WriteLine ("max = " +  x);
+    Console.WriteLine ("min = " + y);
+}
+else
+{
+    Console.WriteLine ("max = " + y);
+    Console.WriteLine ("min = " + x); 
+}
