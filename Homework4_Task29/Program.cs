5a9b80cec8b09ef216ff0089f257b774b13c4f9e
Nastya <stushashilova@tut.by>
2022-09-06 17:57:21 +0300

    Array

---
+++ Homework4_Task29/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.Write ("[");
+void ArrayPrint (int [] arr)
+{
+   for (int i = 0; i < arr.Length ;  i++)
+   {
+      int number = new Random ().Next (0,10);
+      arr [i]= number;
+      if (i >=0 && i <= arr.Length-2)
+      Console.Write (arr [i]+ ",");
+      else 
+      Console.Write (arr [i]);
+   }
+   Console.Write ("]");
+}
+int [] array = new int [8];
+ArrayPrint (array);
+
