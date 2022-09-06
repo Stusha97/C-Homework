96a523a8b6ce73b72dfaabc21eb9cd342057a277
Nastya <stushashilova@tut.by>
2022-09-06 17:54:34 +0300

    Sum of elements in number

---
+++ Homework4_Task27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+void Sum (int a)
+{
+     int len = Convert.ToString (a).Length;
+     int sum = 0;
+
+     for (int i = 0; i < len; i++)
+{
+     sum += a % 10;
+     a = a/10;
+} 
+ Console.WriteLine ($"Сумма цифр данного числа = {sum}");
+}
+Console.WriteLine ("Введите число :");
+int N = Convert.ToInt32 (Console.ReadLine());
+Sum (N);
+
