f9cda2ac7d1f8ffbd0f3da4e3f20ce8a8a513e8e
Nastya <stushashilova@tut.by>
2022-09-17 16:05:10 +0300

    x,y

---
+++ Homework6_43/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+double Read (string message)
+{
+    Console.Write(message);
+    return Convert.ToDouble (Console.ReadLine());
+}
+
+
+void FindPoint (double k1, double k2, double b1, double b2)
+{
+    //double [] point = new double [2];
+    if (k1 == k2)
+    Console.WriteLine ("Прямые не пересекаются");
+    else
+    {
+        double x = (b2-b1)/ (k1-k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+    }  
+}
+double k1 = Read ("Введите k1:");
+double k2 = Read ("Введите k2:");
+double b1 = Read ("Введите b1:");
+double b2 = Read ("Введите b2:");
+FindPoint(k1, k2, b1, b2);
+
+
+
