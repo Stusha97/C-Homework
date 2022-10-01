eed488f1813ff00ef30668005180c28fa7b1015e
Nastya <stushashilova@tut.by>
2022-10-01 19:06:30 +0300

    Nums between M and N

---
+++ Homework9_Task64/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+int Read(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+void ShowNums(int m, int n)
+{
+    for (int i = m; i <= n; i++)
+    {
+        Console.Write(i + ",");
+    }
+}
+int M = Read("Введите M:");
+int N = Read("Введите N:");
+ShowNums(M, N);
+
